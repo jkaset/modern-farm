6e96eb616c4afeda5d6293f3c39eee3759a250d8
Jasmin Kaset <kasetski@gmail.com>
2020-10-08 15:58:05 -0500

    started unworking function in field js

---
+++ src/scripts/field.js
@@ -0,0 +1,15 @@
+let plants = []
+
+export const addPlant = () => {
+  const plantObject = [];
+  for (let i = 0; i < plants.length; i++) {
+    
+    addPlant.push(plantObject)
+  }
+  return addPlant
+}
+
+export const usePlants = () => {
+  return p
+}
+
