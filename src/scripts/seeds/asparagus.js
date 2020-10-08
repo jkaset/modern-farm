002ce42547822855b7c0051a71983104a3337258
Jasmin Kaset <kasetski@gmail.com>
2020-10-08 11:38:46 -0500

    created all seed js files  and tested function for asparagus.js

---
+++ src/scripts/seeds/asparagus.js
@@ -0,0 +1,16 @@
+export const createAsparagus = () => {
+  return `
+  type: "asparagus",
+  height: 24,
+  output: 4
+  `
+}
+
+
+// return `
+// <section class="asparagus">
+//   <div class="type">${asparagus.type}</div>
+//   <div class="height">${asparagus.height}</div>
+//   <div class="output">${asparagus.output}</div>
+// </section>
+// `
